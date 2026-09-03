6d8a8262f8c6a823149685c91678429682763e13
KippoCB <vp.salokannel@gmail.com>
2026-09-03 18:07:51 +0300

    Started work on the HAL

---
+++ OS/base/HAL/hal.h
@@ -0,0 +1,39 @@
+//========== Copyright (C) KukkulaOS 2026, All Rights Reserved ==========
+//
+// File:
+//      hal.h
+//
+// Purpose:
+//      Contains the HAL interface the kernel sees. It will handle the
+//      low level hardware operations specific to different processors
+//      and processor architectures. The kernel only sees what hal.dll 
+//      and hal.h expose but it will never have to interact with the 
+//      actual hardware underneath.
+//
+// Edits:
+//       KippoCB
+//             * Create | 3.9.2026
+//
+//=======================================================================
+#ifndef HAL_H
+#define HAL_H
+
+#include <nttypes.h>
+
+//
+// Routine to read a BYTE from a port
+BYTE NTAPI hal_readPortByte(WORD port);
+
+//
+// Routine to write a BYTE to a port
+BYTE NTAPI hal_writePortByte(WORD port, BYTE value);
+
+//
+// Routine to disable interrupts
+VOID NTAPI hal_disableInterrupts();
+
+//
+// Routine to enable interrupts
+VOID NTAPI hal_enableInterrupts();
+
+#endif // HAL_H
--- OS/base/nttypes.h
+++ OS/base/nttypes.h
@@ -1,14 +1,32 @@
+//========== Copyright (C) KukkulaOS 2026, All Rights Reserved ==========
+//
+// File:
+//      nttypes.h
+//
+// Purpose:
+//      Contains the basic datatypes used systemwide. Also contains the
+//      standard kernel function calling convention using __stdcall
+//
+// Edits:
+//       KippoCB
+//             * Create | 3.9.2026
+//
+//=======================================================================
 #ifndef NTTYPES_H
 #define NTTYPES_H
 
 //
 // Numerical types to represent values like counters or counts.
+//
+// LONG is 32bits as windows has a 32 bit long and 64bit long long int
 typedef char CHAR;
 typedef unsigned char UCHAR;
 typedef short SHORT;
 typedef unsigned short USHORT;
 typedef int INT;
 typedef unsigned int UINT;
+typedef int LONG;
+typedef unsigned int ULONG;
 typedef long long int LONGLONG;
 typedef unsigned long long int ULONGLONG;
 
@@ -36,7 +54,14 @@ typedef void VOID;
 typedef void* LPVOID;
 
 //
-// NT syscall
+// NT status code. This code will be returned from the operations we can do during the system's runtime
+typedef LONG NTSTATUS;
 
+//
+// Standard NT calling convention for kernel functions
+//
+// This will push the arguments of an function from right to left(so VOID NTAPI foo(BYTE a, BYTE b, BYTE c) will be stored in the order c -> b -> a)
+// The compiler will also alter the name of the function in the final combined code to _foo(a, b, c)@12
+#define NTAPI __attribute__((stdcall))
 
 #endif // NTTYPES_H
